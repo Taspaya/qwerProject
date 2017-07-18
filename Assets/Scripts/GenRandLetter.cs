71e298be7b2727b0567ef431d809feb0008a9b05
Taspaya <taspayaa@gmail.com>
2017-07-18 16:44:01 +0200

    pause buttons
    almost clear

--- Assets/Scripts/GenRandLetter.cs
+++ Assets/Scripts/GenRandLetter.cs
@@ -19,9 +19,15 @@ public class GenRandLetter : MonoBehaviour {
 
     public Image sliderColor;                 // Slider color for feedback
     public GameObject lvlchanger;             // imagen de transicion para canviar de nivel
-    public GameObject inGaMenu;               // game menu  
 
+    // game menu  
+    public GameObject inGaMenu;               // game pad menu  
+    public Button resumeBTN;                  // Return button
+    public GameObject estadistiques;          // Statistics screen
+
+    // Keys
     public GameObject[] Tecles;
+    
     // Time variables
     public float lifeTime;             // Variable tiempo ""vida""
     public float keyActiveTime;        // Variable tiempo que esta activa la tecla
@@ -37,9 +43,12 @@ public class GenRandLetter : MonoBehaviour {
     public int vides;                  
     public bool generated;             // Variable Bool que controla si hay alguna tecla generada en la pantalla en el momento
     public bool paused = false;        // Variable for pausing the game
-  
+    public int encerts;
+    public int errors;
+    public int accuracy;  
+    // texts
     public Text lvlText;               // Texto que nos informa del nivel al que estamos
-  
+
 
     [SerializeField]                    
     public Slider progressBar;
@@ -51,8 +60,8 @@ public class GenRandLetter : MonoBehaviour {
         Tecles = new GameObject[8] { Q, W, E, R , D, F, uno, dos};      // Array que contiene las teclas que se van a generar
         generated = false;
         lvlupTime = 0;
-        inGaMenu.SetActive(false);
-
+        inGaMenu.SetActive(false);                                      // Game pause starts at off mode
+        estadistiques.SetActive(false);                                 // Statistics canvas starts at off mode
         lvlText.text = "Lvl: " + level;                                 // Inicialicamos el texto del nivel en el "HUD"
 
         sliderColor.GetComponent<Image>().color = new Color32(50, 50, 50, 255); // Inicialicamos la imagen del slider en negro
@@ -82,20 +91,20 @@ public class GenRandLetter : MonoBehaviour {
         set_a_getKey();                                                 // Generamos letra y posiciones aleatorias
 
 
-        setTimeToZero();                                             // Nos aseguramos que la barra de vida no baja de 0
+        setTimeToZero();                                                // Nos aseguramos que la barra de vida no baja de 0
 
-        if (lifeTime >= LastScore)  // Si la score aconseguida es mayor o igual a la score maxima:
+        if (lifeTime >= LastScore)                                      // Si la score aconseguida es mayor o igual a la score maxima:
         {
-            lvlUP();                // en caso de subir de nivel se ejecuta
+            lvlUP();                                                    // en caso de subir de nivel se ejecuta
             lvlupTime = 0;
             while (lvlupTime < 20000)
             {
                 lvlupTime += Time.deltaTime;
             }
-            lvlchanger.SetActive(false);                 // activamos la imagen de transación de nivel
+            lvlchanger.SetActive(false);//  **********                  // activamos la imagen de transación de nivel
         }
 
-        if (level == 3)                                                  // Augmentamos la dificultad
+        if (level == 3)                                                  // Aumentamos la dificultad
         {
             nTecles = 6;
         }
@@ -113,50 +122,63 @@ public class GenRandLetter : MonoBehaviour {
             level++;                                    // augmenta en 1 el nivel          
     }
 
-    void setTimeToZero()
+    public void resume_BTN() {                          // If the resume button of the pause canvas is pressed...   
+        
+        paused = !paused;                               // Unpause the game
+        Time.timeScale = 1;                             // activate the time
+        inGaMenu.SetActive(false);                      // disable the canvas pause
+
+    }
+
+    public void estadistiques_BTN() {
+
+        estadistiques.SetActive(true);
+
+    }
+
+    void setTimeToZero()                                // If the time wents negative, its set to 0 
     {
         if (lifeTime <= 0)
         {
             lifeTime = 0;
         }
     }
-    float maxScoreCalc() {
+
+
+    float maxScoreCalc() {                              //regulates the difficulti of the next lvl 
 
         if (level <= 3) {
-            return LastScore + (LastScore * 0.2f);
+            return LastScore + (LastScore * 0.2f);      // the max score increases by 0.2 if lvl 3 is not reached
         }
         else
         {
 
-            return LastScore + (LastScore * 0.05f);
+            return LastScore + (LastScore * 0.05f);     // if lvl 3 its reached then the max score increases by 0.05
         }
        
     }
     
-    void updateUI() {
-
-        lvlText.text = "Lvl: " + level;
-     
-
+    void updateUI() {                                  // Updates the lvl number of the screen
+        lvlText.text = "Lvl: " + level;                 // changes the text
     }
 
-    void generateRandomA() { 
+    void generateRandomA() {                            // Generates the random letter
 
-        if (!generated)
+        if (!generated)                                 // if its not already generated...
         {
-            for (int i = 0; i < 8; i++)  // DESACTIVEM LES TECLES AL GENERAR UNA TECLA NOVA
+            for (int i = 0; i < 8; i++)                 // Disables all the letters generated before generating a new one
             {
 
-                Tecles[i].SetActive(false);
+                Tecles[i].SetActive(false);             // Disables all the components of the array where are all the letters.
             }
 
-            a = Random.Range(0, nTecles);
-            posX = Random.Range(-8, 8);
-            posY = Random.Range(0, -7.3f);
-            generated = true;
+            a = Random.Range(0, nTecles);               // generates a number fom 0 to "numbes of letters (value depends on the lvl)"
+            posX = Random.Range(-8, 8);                 // generates a random number for the transform X (into the main camera)
+            posY = Random.Range(0, -7.3f);              // generates a random number for the transform Y (into the main camera)
+            generated = true;                           
            
-            Tecles[a].SetActive(true);
-            Tecles[a].transform.position = new Vector2(posX, posY);
+            Tecles[a].SetActive(true);                  // Activates the letter generated pseudo-randomly
+            Tecles[a].transform.position = new Vector2(posX, posY); // we position the generated letter on the pseudo-random generated positions
         }
 
 
